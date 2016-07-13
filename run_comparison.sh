60d0dbc410a924611596f32ef878633b75f99f79
sbhat <sbhat@fnal.gov>
2016-07-13 15:54:42 -0500

    Amending

--- run_comparison.sh
+++ run_comparison.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Meant to be run from fermicloud049
+# Meant to be run from fermicloud049.  This script reads from a file readin.txt that doesn't exist in the repo for security reasons.
 
 WORD=`awk '{print $1}' readin.txt`
 cd /home/sbhat/CompareRecords && ./comparerecordcounts.py -p $WORD 
